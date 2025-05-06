37e99874f2d39e8132155883885d63971e2da0bd
jrcastropy <jayrcastro.py@gmail.com>
2025-05-06 16:10:47 +0800

    removed req

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
-# Read the contents of requirements.txt and make it a list
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
 
 # Setting up
 setup(
@@ -15,7 +12,18 @@ setup(
     long_description_content_type="text/markdown",
     long_description='A package for using aicmo functions and tools, includes scraping, openai with an options where you can use it in a serverless application such as AWS Lambda and GCP Cloud Function',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        "openai==1.75.0",
+        "scrapingbee==2.0.1",
+        "requests==2.32.3",
+        "boto3==1.37.37",
+        "tiktoken==0.9.0",
+        "opencv-python-headless==4.11.0.86",
+        "beautifulsoup4==4.13.4",
+        "numpy==2.2.4",
+        "python-dotenv==1.1.0",
+        "typesense==1.0.3"
+    ],
     keywords=[
         'aicmo'
     ],
